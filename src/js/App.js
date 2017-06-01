12fb71a54b4befa87a034cdf991b8703c949ca4a
trentspi <tspice23@att.net>
2017-06-01 11:56:51 -0400

    Modified header / navbar

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavBar from './NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+      <MuiThemeProvider>
+        <NavBar />
+      </MuiThemeProvider>
       </div>
     );
   }
---
+++ src/js/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+/**
+ * A simple example of `AppBar` with an icon on the right.
+ * By default, the left icon is a navigation-menu.
+ */
+const NavBar = () => (
+  <AppBar
+    title="Fuel Calculator"
+    style={{ backgroundColor: {red500} }}
+  />
+);
+
+export default NavBar;
